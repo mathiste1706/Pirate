9433e443d5be060cd2fb646497ca84750ff9ced6
mathilde <mathilde1706.lalanne@gmail.com>
2024-03-05 19:21:34 +0100

    Case: attributs, getter, constructeur

--- src/jeu/Case.java
+++ src/jeu/Case.java
@@ -1,5 +1,21 @@
 package jeu;
 
 public class Case {
+	private int numero=0;
+	private Effet effet;
 
+	public Case(Effet effet) {
+		numero++;
+		this.effet=effet;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public Effet getEffet() {
+		return effet;
+	}
+	
+	
 }
