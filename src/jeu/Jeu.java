be7afe1461751094d7590294777aadad784cb363
mathilde <mathilde1706.lalanne@gmail.com>
2024-03-05 16:09:40 +0100

    Jeu: attributs, getter et constructeur

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,5 +1,25 @@
 package jeu;
 
-public class Jeu {
 
+
+import personnages.Pirate;
+
+public class Jeu {
+	private int nbJoueurs=2;
+	private Pirate[] listePirates;
+	
+	
+	public Jeu(int nbJoueurs){
+		this.nbJoueurs=nbJoueurs;
+		listePirates=new Pirate[nbJoueurs];
+	}
+	public Jeu() {
+		listePirates=new Pirate[nbJoueurs];
+	}
+	public int getnbJoueurs() {
+		return nbJoueurs;
+	}
+	
+	
+	
 }
