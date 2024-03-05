0e69cc46cdc7416ffd14b24cc7cc6868dfa080ba
mathilde <mathilde1706.lalanne@gmail.com>
2024-03-05 18:24:28 +0100

    Plateau: attributs, getter, constructeurs

--- src/jeu/Plateau.java
+++ src/jeu/Plateau.java
@@ -1,5 +1,28 @@
 package jeu;
 
+
 public class Plateau {
 
+	private int nbCases=30;
+	private Case[] listeCases;
+	
+	protected Plateau() {
+		listeCases=new Case[nbCases];
+		for (int i=0;i<listeCases.length;i++) {
+			listeCases[i]=new Case(Effet.SANS);
+		}
+	}
+	protected Plateau(int nbCases, Case[] listeCases) {
+		this.nbCases=nbCases;
+		this.listeCases=listeCases;
+	}
+	public int getNbCases() {
+		return nbCases;
+	}
+	public Case[] getListeCases() {
+		return listeCases;
+	}
+	
+	
 }
+ 
