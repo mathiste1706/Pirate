11b0aae83ea2cd9116e25e321dcab851f46ea2da
mathilde <mathilde1706.lalanne@gmail.com>
2024-03-05 19:12:55 +0100

    Arme: attributs, getters, constructeur

--- src/personnages/Arme.java
+++ src/personnages/Arme.java
@@ -2,4 +2,19 @@ package personnages;
 
 public class Arme {
 
+	private int force;
+	private String nom;
+	
+	public Arme(int force, String nom) {
+		this.force = force;
+		this.nom = nom;
+	}
+	public int getForce() {
+		return force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	
 }
