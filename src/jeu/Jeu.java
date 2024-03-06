c4806b9fb5fd42c06be498fde1c0692febfba262
mathilde <mathilde1706.lalanne@gmail.com>
2024-03-06 22:10:16 +0100

    Jeu: ajout des getters manquants, des methodes lanceDe, deplacerPirate et developpement en cours de la methode start

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,13 +1,16 @@
 package jeu;
 
+import java.util.Random;
 
-
+import personnages.Couleur;
+import personnages.Nom;
 import personnages.Pirate;
 
 public class Jeu {
 	private int nbJoueurs=2;
 	private Pirate[] listePirates;
-	
+	private Plateau plateau=new Plateau();
+	private Random random=new Random();
 	
 	public Jeu(int nbJoueurs){
 		this.nbJoueurs=nbJoueurs;
@@ -20,6 +23,32 @@ public class Jeu {
 		return nbJoueurs;
 	}
 	
+	public Pirate[] getListePirates() {
+		return listePirates;
+	}
 	
+	public Plateau getPlateau() {
+		return plateau;
+	}
+	
+	public void start() {
+		
+		
+		for (int i=0; i<nbJoueurs;i++) {
+			listePirates[i]=new Pirate(Nom.values()[i].toString(), Couleur.values()[i]);
+		}
+	}
+	
+	public int lanceDe() {
+		return random.nextInt(6)+1;
+	}
+	
+	public void deplacerPirate(Pirate pirate, int valeurDe) {
+		int nvNum=pirate.getPosition()+valeurDe;
+		if (nvNum>plateau.getNbCases()) {
+			nvNum=plateau.getNbCases()-(nvNum-plateau.getNbCases());
+		}
+		pirate.setPosition(nvNum);
+	}
 	
 }
